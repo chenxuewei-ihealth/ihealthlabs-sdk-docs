7f5901d3316b2efddda2733c1a8dabe3ef25b005
chenxuewei-ihealth <chenxuewei@ihealthlabs.com>
2021-10-14 14:49:03 +0800

    remove build files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/chenxuewei/Documents/work/doc.sdk.ihealthlabs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/chenxuewei/Documents/work/doc.sdk.ihealthlabs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/chenxuewei/Documents/work/doc.sdk.ihealthlabs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/chenxuewei/Documents/work/doc.sdk.ihealthlabs/src/css/custom.css"),
-];
--- build/assets/js/55960ee5.b852d482.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdoc_sdk=self.webpackChunkdoc_sdk||[]).push([[121],{8070:function(s){s.exports=[]}}]);
